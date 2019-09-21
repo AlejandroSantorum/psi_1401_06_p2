f793ba333233cffaf5bd8ca7df8d400f60644dc4
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-09-21 13:29:53 +0200

    add pillow to requirements + add tests

--- django_basics_exercises/tango_with_django_project/rango/tests.py
+++ django_basics_exercises/tango_with_django_project/rango/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import rango.tests_utils as util
 
-# Create your tests here.
+# uncoment next line to run GeneralTests, IndexPageTests, AboutPageTests tests
+from rango.tests_first_day import GeneralTests, IndexPageTests, AboutPageTests
+# from rango.tests_second_day import Chapter5ViewTests, ModelTests
+#from rango.tests_second_day import Chapter5ViewTests, ModelTests
+
+# These tests assume User Autentication has been created
+#from rango.tests_third_day import Chapter6FormTests
+#from rango.tests_fourth_day import UserAuthenticationTests
