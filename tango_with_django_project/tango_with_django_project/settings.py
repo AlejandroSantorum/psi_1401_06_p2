2a5412ac82a50fb291b1e53591d57824eaedaf39
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-09-27 10:31:21 +0200

    fixed about page test and updated requirements

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -82,7 +82,6 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
