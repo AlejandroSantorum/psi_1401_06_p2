af4c7c962daba9c7dce6aa11d0711049c1096b44
Lletfrix <lletfrix@gmail.com>
2019-10-04 18:53:09 +0200

    finish Forms chapter

---
+++ tango_with_django_project/rango/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128,
+                           help_text='Please, enter category name')
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Category # This associates the form with the model
+
+        # This specifies which fields we want to use on the form
+        fields = ('name',)
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128,
+                            help_text='Please, enter page\'s title')
+    url = forms.URLField(max_length=200,
+                         help_text='Please, enter page\'s URL')
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+
+        # This specifies that we dont want to use category field,
+        # its equivalent to fields = ('title', 'url', 'views')
+        exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        # Get will return None if no key is present on the dict
+        # cleaned_data['url'] would raise KeyError instead
+        url = cleaned_data.get('url')
+
+        # Check if URL is well-formed and solve errors
+        # Should check also https://
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
